73ed5f9141e57e1e5c0bd53fd9e977c4138be4b6
Wilson You <wayneyou@126.com>
2021-04-05 23:25:54 -0600

    funcs added

--- script.js
+++ script.js
@@ -21,8 +21,27 @@ pEvery.innerHTML = arr1.every(aboveEight);
 //function fill; return a modified array
 const arr2 = [1, 2, 3, 4, 5]
 const pFill = document.querySelector('.fill1')
-const filled = arr2.fill(11, 2, 5)
+const filled = arr2.fill(11, 2)
 pFill.innerHTML = filled
-console.log(arr2);
+
+//function filter; return an array with elements passed the callback func
+
+const arr3 = [1, 4, 9, 11, 14]
+const filterFunc = item => item % 2 === 0
+const pFilter = document.querySelector('.filter1')
+pFilter.innerHTML = arr3.filter(filterFunc)
+
+//function find; return the first item, which meets the condition, from an array
+
+const arr4 = ['a', 'ab', 'abc', 'abcd']
+const findItem = item => item.length >= 2
+const pFind = document.querySelector('.find1')
+pFind.innerHTML = arr4.find(findItem)
+
+//function findIndex; return the index of the first item, which meets the condition, from an array
+const arr5 = [1, 3, 5, 9, 11]
+const pFindIndex = document.querySelector('.findindex1')
+const fIndex = item => item === 10
+pFindIndex.innerHTML = arr5.findIndex(fIndex)
 
 
